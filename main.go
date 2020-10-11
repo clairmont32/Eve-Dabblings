ae57958f0dbe159cf1560fcdb050536463a71656
Matthew Clairmont <matthew.clairmont1@gmail.com>
2020-10-10 21:07:33 -0400

    rename for abstraction

--- main.go
+++ main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"Eve-Dabblings/regions"
+	"Eve-Dabblings/universe"
+	//	log "github.com/sirupsen/logrus"
 	"fmt"
 )
 
-func main() {
-
+func getRegionIDs() {
 	regionIDs, err := regions.GetRegionIDs()
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	fmt.Println(regionIDs)
+
+}
+
+func getUniverseID(name string) {
+	data, err := universe.GetUniverseID(name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(data)
+}
+
+func main() {
+
+	//globals.SetupLogging("debug")
+	getUniverseID("The Citadel")
 
 }
